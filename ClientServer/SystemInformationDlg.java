0d287ccf7342938c0280dc8eff2468db99fc59d7
Taylor Hardin <taylorhardin@Taylors-iMac.local>
2015-02-19 01:25:49 -0500

    Fixed bad package bug

--- ClientServer/SystemInformationDlg.java
+++ ClientServer/SystemInformationDlg.java
@@ -1,14 +1,13 @@
-import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 
-public class SystemInformationDlg extends JDialog {
+public class SystemInformationDlg {
 
 	public void about() {
-		String message = "The main window displays the current projects in the database. Use the bottons on the\n"
-				+ "bottom to Create/Edit/Delete Projects. Double click on a project to view/edit it's issue list.\n\n"
-				+ "The issue window displays the current issues for the project selected in the main window. Use the\n"
-				+ "buttons on the bottom to Create/Edit/Delete Issues for the current Project. Double click on an\n"
-				+ "Issue to view its full description.\n\n";
+		String message = "The main window displays the current games in the database. Use the bottons on the\n"
+				+ "bottom to Save or Restore a Game. Double click on a Game to join.\n\n"
+				+ "The Game window will pop up after double clicking a game. Use the action buttons to perform\n"
+				+ "actions like fire missiles, navigate, and scan. Actions made by players will appear in the\n"
+				+ "action log at the bottom.\n\n";
 		
 		JOptionPane.showMessageDialog(null,  message, "About Information", JOptionPane.PLAIN_MESSAGE);
 	}
