07c13859b5b37d220e49152ee641dd1d83e42f82
Taylor Hardin <taylorhardin@59.246.21.198.tigernet.wifi.dyn.clemson.edu>
2015-02-19 09:18:19 -0500

    Added Authentication Dialog

--- ClientServer/MainDriver.java
+++ ClientServer/MainDriver.java
@@ -1,6 +1,8 @@
 public class MainDriver {
 	
 	public static void main(String[] args) {
+		
+		AuthenticationDialog auth = new AuthenticationDialog();
 		MainFrame frame = new MainFrame("Game Application");
 			frame.initFrame();
 	}
